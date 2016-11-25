ccaaa2035f434d581941ac611f2789ad2bce5698
kimxu <kimxu_me@163.com>
2016-11-25 14:32:25 +0800

    fix upload blog bug4

--- upload/views.py
+++ upload/views.py
@@ -30,8 +30,9 @@ def upload():
     try:
         f = request.files["file_data"]
         f_name = secure_filename(f.filename)
-        print(os.path.join(INPUT_CONTENT, f_name))
-        print(os.path.abspath('.'))
+        print('1'+os.path.join(INPUT_CONTENT, f_name))
+        print('2'+os.path.abspath('.'))
+        print('3'+os.path.abspath(__file__))
         f.save(os.path.join(INPUT_CONTENT, f_name))
         generate()
         IndexData.reload_index_data()
