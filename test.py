256ed23dd3657287a9e517c1b2ac28651df0b68b
kimxu <kimxu_me@163.com>
2016-11-25 14:32:52 +0800

    fix upload blog bug5

--- test.py
+++
@@ -1,7 +0,0 @@
-import os,sys
-
-
-dirpath = os.path.abspath(__file__).replace('test.py','')+'in'
-print(dirpath+' kimxu')
-# sys.path.extend([WORKING_DIR_AND_PYTHON_PATHS])
-print("current workdirectory : ", os.getcwd())
